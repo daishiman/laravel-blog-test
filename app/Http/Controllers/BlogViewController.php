82fd4dc4ecedcc44693c4a9772169dd7fd801758
daishiman <manju.manju.03.28@gmail.com>
2022-02-17 21:21:47 +0900

    別ファイルから移行

---
+++ app/Http/Controllers/BlogViewController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogViewController extends Controller
+{
+    public function index()
+    {
+        $blogs = Blog::get();
+
+        return view('index', compact('blogs'));
+
+    }
+}
